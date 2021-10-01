be5fa7853523d26169d8bcd57f40416d3447e329
ucdn-official <91743826+ucdn-official@users.noreply.github.com>
2021-10-01 20:46:03 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var menu = document.getElementById("menu");
+menu.addEventListener("click",function(){
+    document.getElementById("hm").style.display="block"
+})
+var close= document.getElementById("close");
+close.addEventListener("click",function(){
+    document.getElementById("hm").style.display="none"
+});
